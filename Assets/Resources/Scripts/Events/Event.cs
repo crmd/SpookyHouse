bfa48134c89d6e69655550fe24af10d3ddc400e4
Cameron Dorman <cdorm@hotmail.com>
2015-07-23 11:25:07 +1000

    Moved Everything into resources, set up the generator, set up second floor light switches

---
+++ Assets/Resources/Scripts/Events/Event.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event : MonoBehaviour {
+
+	protected static bool powerOn = true;
+
+	public virtual void Start()
+	{
+
+	}
+
+	public virtual void Update()
+	{
+
+	}
+
+	public virtual void Activate()
+	{
+
+	}
+}
---
+++ Assets/Resources/Scripts/Events/EventBlackout.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventBlackout : Event {
+
+	private Trigger trigger;
+
+	public override void Start()
+	{
+		base.Start ();
+		trigger = GetComponent<Trigger> ();
+	}
+
+	public override void Activate()
+	{
+		base.Activate ();
+		powerOn = false;
+		trigger.PowerOff ();
+	}
+}
